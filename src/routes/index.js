97d6f569a992fc0da03314c22f60c9f8dc27023d
gideon <gideonam@yahoo.com.br>
2021-08-19 23:12:03 -0300

    add estrutura de formulario

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,6 +20,10 @@ routes.get('/ads/create', function(req, res) {
     return res.redirect("/products/create") 
 })
 
+routes.get('/accounts', function(req, res) {  
+    return res.redirect("/users/register") 
+})
+
 
 
 module.exports = routes
